5ea09ed68d6628260e6ad30d5680906763aa1586
Manav <19cp.manavtimbadiya@adit.ac.in>
2021-09-02 13:16:32 -0500

    Status Added

--- src/components/TestLetter/TestLetter.jsx
+++ src/components/TestLetter/TestLetter.jsx
@@ -2,8 +2,29 @@ import React from "react";
 import "./TestLetter.css";
 
 function TestLetter({individualLetterInfo}){
+	const {status} = individualLetterInfo;
+//	let statusClass;
+
+	const statusClass ={
+		correct : "test-letter-correct",
+		incorrect : "test-letter-incorrect",
+		notAttempted : "test-letter-not-attempted",
+	}[status];
+
+/*	if(status == "correct")
+	{
+		statusClass = "test-letter-correct";
+	}
+	else if (status == "incorrect")
+	{
+		statusClass = "test-letter-incorrect";
+	}
+	else {
+		statusClass ="test-letter-not-attempted";
+	}
+*/
 	return (
-		<span className="test-letter">{individualLetterInfo.testLetter} </span>
+		<span className={`test-letter ${statusClass}`}>{individualLetterInfo.testLetter} </span>
 		)
 }
 
