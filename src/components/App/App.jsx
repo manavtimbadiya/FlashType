d84970b9eaee91e91917ffe47e4cdcc52423e4e1
Manav <19cp.manavtimbadiya@adit.ac.in>
2021-09-03 15:50:18 -0500

    Underflow Case

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -35,8 +35,15 @@ class App extends React.Component {
         const timer = setInterval(() => {
             if(this.state.timeRemaining > 0)
                 {
+                  //Change the WPM
+                    const timeSpent = TotalTime - this.state.timeRemaining;
+                    const wpm = timeSpent > 0 
+                            ? (this.state.words / timeSpent) * TotalTime
+                            : 0;
+
                     this.setState({
                     timeRemaining: this.state.timeRemaining - 1,
+                    wpm : parseInt(wpm),
                     });    
                 }
             else 
@@ -48,6 +55,41 @@ class App extends React.Component {
 
     handleUserInput = (inputValue) => {
         if(this.state.timeStarted == false) this.startTimer();
+
+         /**
+         * 1. Handle the underflow case - all characters should be shown as not-attempted
+         * 2. Handle the overflow case - early exit
+         * 3. Handle the backspace case
+         *      - Mark the [index+1] element as notAttempted
+         *        (irrespective of whether the index is less than zero)
+         *      - But, don't forget to check for the overflow here
+         *        (index + 1 -> out of bound, when index === length-1)
+         * 4. Update the status in test info
+         *      1. Find out the last character in the inputValue and it's index
+         *      2. Check if the character at same index in testInfo (state) matches
+         *      3. Yes -> Correct
+         *         No  -> Incorrect (Mistake++)
+         * 5. Irrespective of the case, characters, words and wpm can be updated
+         */
+
+         const characters = inputValue.length;
+         const words = inputValue.split(" ").length;
+         const index = characters - 1 ;
+
+         if(index >0)
+         {
+            this.setState({
+                testInfo: [
+                {
+                    testLetter: this.state.testInfo[0].testLetter,
+                    status: "notAttempted",
+                },
+                ...this.state.testInfo.slice(1)
+                ],
+                characters,
+                words,
+            });
+         }
     }
 
     componentDidMount () {   //this is used because fetching paragraph from url take a time so render first call then cDM call so It Doesn't take time to loading page.
